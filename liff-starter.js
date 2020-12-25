2d956336bc4fffc8bb5966fae8be5e5649fb9ce3
rizki_syafapri <rizkisyafapri@gmail.com>
2020-12-25 14:26:42 +0700

    first liff commit

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1655318066-ZoNWMwQk";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1655318066-kKyG6YA8";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
