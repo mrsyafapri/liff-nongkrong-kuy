fae10f075ab14209c9f0d01a53f1357fd3b5b043
rizki_syafapri <rizkisyafapri@gmail.com>
2020-12-29 17:01:34 +0700

    fix

--- liff-starter.js
+++ liff-starter.js
@@ -130,7 +130,7 @@ function registerButtonHandlers() {
     });
 
     // sendMessages call
-    document.getElementById('sendMessageButton').addEventListener('click', function() {
+    document.getElementById('sendMessageButton').addEventListener(trigger('click'), function() {
         if (!liff.isInClient()) {
             sendAlertIfNotInClient();
         } else {
