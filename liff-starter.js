6d81f488f7bb6fe5dc5e34b00bbb3a27d2b7487a
rizki_syafapri <rizkisyafapri@gmail.com>
2020-12-29 16:55:46 +0700

    fix

--- liff-starter.js
+++ liff-starter.js
@@ -95,12 +95,6 @@ function displayIsInClientInfo() {
     }
 }
 
-liff.getProfile().then(function(profile) {
-    const name = profile.displayName;
-}).catch(function(error) {
-    window.alert('Error getting profile: ' + error);
-});
-
 function registerButtonHandlers() {
     // openWindow call
     document.getElementById('openWindowButton').addEventListener('click', function() {
@@ -142,16 +136,7 @@ function registerButtonHandlers() {
         } else {
             liff.sendMessages([{
                 'type': 'text',
-                'text': `Hai ${name},
-
-Terima kasih telah memesan makanan dan minuman di Nongkrong Kuy, berikut adalah review pesanan Anda:
-
-* 0 Makanan
-* 0 Minuman
-
-Pesanan Anda akan segera diproses dan akan diberitahu jika sudah bisa diambil.
-
-Mohon Ditunggu Ya!`
+                'text': 'Hai Customers,\n\nTerima kasih telah memesan makanan dan minuman di Nongkrong Kuy, berikut adalah review pesanan Anda:\n\n* 0 Makanan\n* 0 Minuman\n\nPesanan Anda akan segera diproses dan akan diberitahu jika sudah bisa diambil.\n\nMohon Ditunggu Ya!'
             }]).then(function() {
                 window.alert('Terima kasih sudah memesan! Silahkan cek kembali pesanan Anda pada pesan yang dikirimkan');
             }).catch(function(error) {
